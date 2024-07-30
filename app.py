5047466264719b4c7478a7764734dc54baa0f08f
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 19:18:08 -0400

    added dump.sql

--- app.py
+++ app.py
@@ -1,8 +1,28 @@
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+import mysql.connector
+from mysql.connector import Error
 
 app = Flask(__name__)
 CORS(app) 
+
+config = {
+    'host': '127.0.0.1',
+    'user': 'root',
+    'password': '5V@xqzA1/4qU',
+    'database': 'pledgx',
+    'port': 3306
+}
+
+def create_connection():
+    try:
+        connection = mysql.connector.connect(**config)
+        if connection.is_connected():
+            print("Connected to MySQL database")
+        return connection
+    except Error as e:
+        print(f"Error: {e}")
+        return None
 
 @app.route('/submit-user', methods=['POST'])
 def submit_form():
@@ -19,8 +39,11 @@ def submit_form():
         return jsonify({"Error": "jobTitle must have a value."}), 400    
     if not data["country"]:
         return jsonify({"Error": "country must have a value."}), 400
+    
     return jsonify("Success"), 200
 
 
 if __name__ == '__main__':
+    connection = create_connection()
     app.run(port=3000, debug=True)
+    
