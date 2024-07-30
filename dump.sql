b57f81d237e4aca59444ee659797f8ebe1015132
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 19:46:05 -0400

    created users table in dump

--- dump.sql
+++ dump.sql
@@ -5,3 +5,14 @@ GRANT ALL PRIVILEGES ON *.* TO 'pledgx_user'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 
 CREATE DATABASE IF NOT EXISTS pledgx;
+
+USE pledgx;
+
+CREATE TABLE `users` (
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  `jobTitle` varchar(50) NOT NULL,
+  `country` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
