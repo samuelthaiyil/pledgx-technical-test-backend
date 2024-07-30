d8db0ec37b1ddc17fdbd962729582ecd47713d91
samuelthaiyil <sathaiyi@lakeheadu.ca>
2024-07-30 18:23:26 -0400

    added validation for submit-user api

--- app.py
+++ app.py
@@ -4,6 +4,23 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app) 
 
+@app.route('/submit-user', methods=['POST'])
+def submit_form():
+    data = request.json
+   
+    # validation
+    if not data["firstName"]:
+        return jsonify({"Error": "firstName must have a value."}), 400
+    if not data["lastName"]:
+        return jsonify({"Error": "lastName must have a value."}), 400
+    if not data["phoneNumber"]:
+        return jsonify({"Error": "phoneNumber must have a value."}), 400
+    if not data["jobTitle"]:
+        return jsonify({"Error": "jobTitle must have a value."}), 400    
+    if not data["country"]:
+        return jsonify({"Error": "country must have a value."}), 400
+    return jsonify("Success"), 200
+
 
 if __name__ == '__main__':
     app.run(port=3000, debug=True)
